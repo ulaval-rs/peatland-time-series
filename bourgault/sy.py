7a34de1b384bf7a256052cfe8d7248bd4c768a30
Gabriel Couture <gacou54@gmail.com>
2021-11-04 17:25:22 -0400

    Adding calculate_sy test, and improve calculate_sy

--- bourgault/sy.py
+++ bourgault/sy.py
@@ -10,47 +10,50 @@ def calculate_sy(
         gap: int = 5,
         max_hour: int = 5,
         threshold: float = 0.3,
-        resample: Union[pandas.DateOffset, pandas.Timedelta, str] = "H") -> pd.DataFrame:
-    """ Calculate Sy
+        resample: Union[pandas.DateOffset, pandas.Timedelta, str] = 'H') -> pd.DataFrame:
+    """Calculate Sy, the profile of effectives porosity.
 
     Parameters
     ----------
     data : pandas.Dataframe
         DataFrame of time series with at least the 3 following columns: 'date', 'data_wtd' and 'data_prec'.
         'date' refers to the date of the data acquisition ("YYYY-MM-DD hh:mm:ss", ex. "2011-06-15 15:00:00").
-        'data_wtd' refers to the measure TODO.
+        'data_wtd' refers to the water table depth to the surface.
         'data_prec' refers to the precision of the measure.
     gap : int
-        TODO
+        Time which makes it possible to isolate rainy events.
+        For example, if it does not rain for 6 hours and the gap parameter is equal to 5,
+        the precipitation will be separated into two rainy events.
     max_hour : int
-        Maximum hour to consider.
+        Parameter that limits the search for the maximum in the water level after a precipitation.
     threshold : float
         Precision threshold.
     resample : str
-        Resample rule. See pandas.DataFrame.resample doc for more details.
+        Resample rule for the aggregation step. See pandas.DataFrame.resample doc for more details.
 
     Returns
     -------
     pandas.DataFrame
-        TODO
+        Profile of effectives porosity.
     """
     ####### DEFINE_DATA ########
-    time = data["date"].astype('datetime64[ns]')
-    wtd = data["data_wtd"]
-    prec = data["data_prec"]
+    time = data['date'].astype('datetime64[ns]')
+    water_table_depth = data['data_wtd']
+    precision = data['data_prec']
 
     ####### TRANSFORM_IN_PANDAS_DATAFRAME #######
-    df_wtd = pd.DataFrame(wtd)
-    df_wtd.index = time
-    df_prec = pd.DataFrame(prec)
-    df_prec.index = time
+    df_water_table_depth = pd.DataFrame(water_table_depth)
+    df_water_table_depth.index = time
+
+    df_precision = pd.DataFrame(precision)
+    df_precision.index = time
 
     ####### RESAMPLE DATA #########
-    df_wtd = df_wtd.resample(resample).mean()
-    df_prec = df_prec.resample(resample).sum()
+    df_water_table_depth = df_water_table_depth.resample(resample).mean()
+    df_precision = df_precision.resample(resample).sum()
 
     ####### FIND PRECIPITATION EVENTS #########
-    position60 = np.array(np.where(df_prec.data_prec.values > threshold)).squeeze()
+    position60 = np.array(np.where(df_precision.data_prec.values > threshold)).squeeze()
     diff60 = np.diff(position60)
     gap60 = np.array(np.where(diff60 >= gap)).squeeze()
     gap61 = np.insert(gap60, 0, -1)
@@ -58,87 +61,78 @@ def calculate_sy(
 
     ####### ISOLATION_PRECIPITATION_EVENT ######
     list_event = list()
-    sommeprec = np.zeros(len(gap61))
+    precision_sum = np.zeros(len(gap61))
     for j in range(0, len(gap61)):
-        # j=2
-        prec_event = position60[gap61[j] + 1:gap60[j] + 1]
-        list_event.append(prec_event)
-        sommeprec[j] = df_prec.iloc[prec_event].sum().values
+        precision_event = position60[gap61[j] + 1:gap60[j] + 1]
+        list_event.append(precision_event)
+
+        precision_sum[j] = df_precision.iloc[precision_event].sum().values
 
     ####### BEGIN_END_NB_EVENTS #######
-    nb_event = len(sommeprec)
-    debut = position60[gap61 + 1]
-    fin = position60[gap60]
-    date_debut = df_prec.iloc[position60[gap61 + 1]].index
-    date_fin = df_prec.iloc[position60[gap60]].index
+    beginning = position60[gap61 + 1]
+    end = position60[gap60]
+    dates_beginning = df_precision.iloc[position60[gap61 + 1]].index
+    dates_ending = df_precision.iloc[position60[gap60]].index
 
     ######## CREATE_MATRIX_FOR_MIN_MAX_IDENTIFICATION ########
-    wtd_list = list()
+    water_table_depths = list()
     idx_max = list()
     idx_min = list()
 
-    for i in range(0, len(debut)):
-        wtd_list.append(df_wtd.iloc[debut[i]:fin[i] + max_hour].values.squeeze())
-        idx_max.append(df_wtd.iloc[debut[i]:fin[i] + max_hour].idxmax().tolist()[0])
-        idx_min.append(df_wtd.iloc[debut[i]:fin[i] + max_hour].idxmin().tolist()[0])
+    for i in range(0, len(beginning)):
+        water_table_depths.append(df_water_table_depth.iloc[beginning[i]:end[i] + max_hour].values.squeeze())
+        idx_max.append(df_water_table_depth.iloc[beginning[i]:end[i] + max_hour].idxmax().tolist()[0])
+        idx_min.append(df_water_table_depth.iloc[beginning[i]:end[i] + max_hour].idxmin().tolist()[0])
 
     ######## CALCULATE MIN_MAX_WTD ########
-    max_wtd = np.array([i.max() for i in wtd_list])
-    min_wtd = np.array([i.min() for i in wtd_list])
+    max_wtd = np.array([i.max() for i in water_table_depths])
+    min_wtd = np.array([i.min() for i in water_table_depths])
 
     ######## ACCURACY CALCULATION ########
     wtd_list_it_all = []
-    for j in [*range(5, 15)]:
+    for j in range(5, 15):
         wtd_list_it = list()
 
-        for i in range(0, len(debut)):
-            wtd_list_it.append(df_wtd.iloc[debut[i]:fin[i] + j].values.squeeze())
+        for i in range(0, len(beginning)):
+            wtd_list_it.append(df_water_table_depth.iloc[beginning[i]:end[i] + j].values.squeeze())
 
         max_wtd_it = [i.max() for i in wtd_list_it]
         wtd_list_it_all.append(max_wtd_it)
 
-    accuracy_mean = (pd.DataFrame(wtd_list_it_all).diff(axis=0).mean(axis=0)).values
-    accuracy_std = (pd.DataFrame(wtd_list_it_all).diff(axis=0).std(axis=0)).values
+    accuracy_means = pd.DataFrame(wtd_list_it_all).diff(axis=0).mean(axis=0).values
+    accuracy_stds = pd.DataFrame(wtd_list_it_all).diff(axis=0).std(axis=0).values
 
     ######## SY_CALCULATION_AND_PREC_INTENSITY ########
-    #### Module Ajouter pour tenir compte des précipitations rapides ######
-    dur = []
-    for k in range(0, len(date_debut)):
-        if ((date_fin[k] - date_debut[k]).seconds) == 0:
-            dur.append(0.5)
+    # Added to account for rapid precipitation
+    durations = []
+    for date_beginning, date_ending in zip(dates_beginning, dates_ending):
+        if (date_ending - date_beginning).seconds == 0:
+            durations.append(0.5)
         else:
-            dur.append((date_fin[k] - date_debut[k]).seconds // 3600)
-    #  else:
-    #    dur.append(date_fin[i]-date_debut[i]).seconds//3600
-    #  print((date_fin[i]-date_debut[i]).seconds)
+            durations.append((date_ending - date_beginning).seconds // 3600)
 
-    duree = np.array(dur)
-    intensite = sommeprec / duree
+    durations_array = np.array(durations)
+    intensities = precision_sum / durations_array
     delta_h = max_wtd - min_wtd
-    sy = (sommeprec / delta_h) / 1000
+    sy = (precision_sum / delta_h) / 1000
     depth = (max_wtd + min_wtd) / 2
 
     ######## CREATE SUMMARY TABLE ########
     summary_table = pd.DataFrame({
-        "date_debut": date_debut,
-        "date_fin": date_fin,
-        "sommeprec": sommeprec,
-        "max_wtd": max_wtd,
-        "min_wtd": min_wtd,
-        "duree": dur,
-        "intensite": intensite,
-        "delta_h": delta_h,
-        "depth": depth,
-        "sy": sy,
-        "idx_max": idx_max,
-        "idx_min": idx_min,
-        "accuracy_mean": accuracy_mean,
-        "accuracy_std": accuracy_std
+        'date_beginning': dates_beginning,
+        'date_ending': dates_ending,
+        'precision_sum': precision_sum,
+        'max_wtd': max_wtd,
+        'min_wtd': min_wtd,
+        'durations': durations,
+        'intensities': intensities,
+        'delta_h': delta_h,
+        'depth': depth,
+        'sy': sy,
+        'idx_max': idx_max,
+        'idx_min': idx_min,
+        'accuracy_mean': accuracy_means,
+        'accuracy_std': accuracy_stds
     })
 
     return summary_table
-
-
-# Function to calculate the power-law with constants a and b
-def power_law(x, a, b):
-    return a * np.power(x, b)
--- tests/test_sy.py
+++ tests/test_sy.py
@@ -1,9 +1,14 @@
+import numpy
 import pandas
 import pytest
 
 from bourgault.sy import calculate_sy
 
 TIME_SERIES_PATH = './tests/data/time_series/time_series/ahlenmoor/ahlenmoor_af_naturnah_sp.csv'
+EXPECTED_RESULT_PATH = './tests/data/expected_results.csv'
+EXPECTED_COLUMNS = ['date_beginning', 'date_ending', 'precision_sum', 'max_wtd', 'min_wtd',
+                    'durations', 'intensities', 'delta_h', 'depth', 'sy', 'idx_max', 'idx_min',
+                    'accuracy_mean', 'accuracy_std']
 
 
 @pytest.fixture
@@ -11,8 +16,22 @@ def time_series():
     return pandas.read_csv(TIME_SERIES_PATH)
 
 
-def test_calculate_sy(time_series):
-    result = calculate_sy(time_series)
-    print(result)
+@pytest.fixture
+def expected_result():
+    return pandas.read_csv(EXPECTED_RESULT_PATH)
 
-    assert result == False
+
+def test_calculate_sy(time_series, expected_result):
+    result = calculate_sy(time_series)
+
+    for expected_column in EXPECTED_COLUMNS:
+        assert expected_column in result.columns
+
+    for i, j in zip(expected_result['sy'].values, result['sy'].values):
+        if numpy.isnan(i) and numpy.isnan(j):
+            continue
+
+        if numpy.isinf(i) and numpy.isinf(j):
+            continue
+
+        assert abs(i - j) < 1e-15  # Ignoring numerical difference (~1e-16)
